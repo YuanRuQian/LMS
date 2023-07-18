5758ce79cca2f8d4ffcd46bfcd26f36478d387b7
Lydia Yuan <lydiayuan99@gmail.com>
2023-07-17 20:15:10 -0600

    add: student controller

--- LMSHandout/LMS/Controllers/StudentController.cs
+++ LMSHandout/LMS/Controllers/StudentController.cs
@@ -58,9 +58,6 @@ namespace LMS.Controllers
             return View();
         }
 
-
-        /*******Begin code to modify********/
-
         /// <summary>
         /// Returns a JSON array of the classes the given student is enrolled in.
         /// Each object in the array should have the following fields:
@@ -74,8 +71,26 @@ namespace LMS.Controllers
         /// <param name="uid">The uid of the student</param>
         /// <returns>The JSON array</returns>
         public IActionResult GetMyClasses(string uid)
-        {           
-            return Json(null);
+        {
+            var student = db.Students.FirstOrDefault(s => s.Uid == uid);
+            if (student == null)
+            {
+                return Json(new List<object>());
+            }
+
+            var enrollments = db.Enrollments
+                .Where(e => e.StudentId == uid)
+                .Select(e => new
+                {
+                    subject = e.Class.Course.Department,
+                    number = e.Class.Course.Number,
+                    name = e.Class.Course.Name,
+                    season = e.Class.Season,
+                    year = e.Class.Year,
+                    grade = e.Grade ?? "--"
+                }).ToList();
+
+            return Json(enrollments);
         }
 
         /// <summary>
@@ -93,10 +108,31 @@ namespace LMS.Controllers
         /// <param name="uid"></param>
         /// <returns>The JSON array</returns>
         public IActionResult GetAssignmentsInClass(string subject, int num, string season, int year, string uid)
-        {            
-            return Json(null);
-        }
+        {
+            var student = db.Students.FirstOrDefault(s => s.Uid == uid);
+            if (student == null)
+            {
+                return Json(new List<object>());
+            }
 
+            var assignments = db.Enrollments
+                .Where(e => e.StudentId == uid && e.Class.Course.Department == subject && e.Class.Course.Number == num && e.Class.Season == season && e.Class.Year == year)
+                .Join(db.Assignments,
+                    enrollment => enrollment.ClassId,
+                    assignment => assignment.CategoryId,
+                    (enrollment, assignment) => new
+                    {
+                        aname = assignment.Name,
+                        cname = assignment.Category.Name,
+                        due = assignment.Due,
+                        score = assignment.Submissions.FirstOrDefault(s => s.StudentId == uid) != null
+                            ? assignment.Submissions.First(s => s.StudentId == uid).Score
+                            : null
+                    })
+                .ToList();
+
+            return Json(assignments);
+        }
 
 
         /// <summary>
@@ -117,9 +153,66 @@ namespace LMS.Controllers
         /// <param name="contents">The text contents of the student's submission</param>
         /// <returns>A JSON object containing {success = true/false}</returns>
         public IActionResult SubmitAssignmentText(string subject, int num, string season, int year,
-          string category, string asgname, string uid, string contents)
-        {           
-            return Json(new { success = false });
+            string category, string asgname, string uid, string contents)
+        {
+            // Find the student
+            var student = db.Students.FirstOrDefault(s => s.Uid == uid);
+            if (student == null)
+            {
+                return Json(new { success = false });
+            }
+
+            // Find the assignment category
+            var assignmentCategory = db.AssignmentCategories.FirstOrDefault(c =>
+                c.Class.Course.Department == subject &&
+                c.Class.Course.Number == num &&
+                c.Class.Season == season &&
+                c.Class.Year == year &&
+                c.Name == category);
+
+            if (assignmentCategory == null)
+            {
+                return Json(new { success = false });
+            }
+
+            // Find the assignment within the category
+            var assignment = db.Assignments.FirstOrDefault(a =>
+                a.CategoryId == assignmentCategory.Id &&
+                a.Name == asgname);
+
+            if (assignment == null)
+            {
+                return Json(new { success = false });
+            }
+
+            // Find the submission for the student and assignment
+            var submission = db.Submissions.FirstOrDefault(s =>
+                s.StudentId == uid &&
+                s.AssignmentId == assignment.Id);
+
+            // If a submission already exists, update its contents and time
+            if (submission != null)
+            {
+                submission.Contents = contents;
+                submission.Time = DateTime.Now;
+            }
+            else
+            {
+                // Create a new submission
+                submission = new Submission
+                {
+                    StudentId = uid,
+                    AssignmentId = assignment.Id,
+                    Time = DateTime.Now,
+                    Contents = contents,
+                    Score = null
+                };
+                db.Submissions.Add(submission);
+            }
+
+            db.SaveChanges();
+
+            return Json(new { success = true });
         }
 
 
@@ -134,10 +227,54 @@ namespace LMS.Controllers
         /// <returns>A JSON object containing {success = {true/false}. 
         /// false if the student is already enrolled in the class, true otherwise.</returns>
         public IActionResult Enroll(string subject, int num, string season, int year, string uid)
-        {          
-            return Json(new { success = false});
-        }
+        {
+            // Find the student
+            var student = db.Students.FirstOrDefault(s => s.Uid == uid);
+            if (student == null)
+            {
+                return Json(new { success = false });
+            }
 
+            // Find the class
+            var course = db.Courses.FirstOrDefault(c => c.Department == subject && c.Number == num);
+            if (course == null)
+            {
+                return Json(new { success = false });
+            }
+
+            var classObj = db.Classes.FirstOrDefault(c =>
+                c.CourseId == course.Id &&
+                c.Season == season &&
+                c.Year == year);
+
+            if (classObj == null)
+            {
+                return Json(new { success = false });
+            }
+
+            // Check if the student is already enrolled in the class
+            var existingEnrollment = db.Enrollments.FirstOrDefault(e =>
+                e.StudentId == uid &&
+                e.ClassId == classObj.Id);
+
+            if (existingEnrollment != null)
+            {
+                return Json(new { success = false });
+            }
+
+            // Create a new enrollment
+            var enrollment = new Enrollment
+            {
+                StudentId = uid,
+                ClassId = classObj.Id,
+                Grade = null
+            };
+
+            db.Enrollments.Add(enrollment);
+            db.SaveChanges();
+
+            return Json(new { success = true });
+        }
 
 
         /// <summary>
@@ -152,11 +289,75 @@ namespace LMS.Controllers
         /// <param name="uid">The uid of the student</param>
         /// <returns>A JSON object containing a single field called "gpa" with the number value</returns>
         public IActionResult GetGPA(string uid)
-        {            
-            return Json(null);
+        {
+            // Find the student
+            var student = db.Students.FirstOrDefault(s => s.Uid == uid);
+            if (student == null)
+            {
+                return Json(new { gpa = 0.0 });
+            }
+
+            // Find the enrollments for the student
+            var enrollments = db.Enrollments.Where(e => e.StudentId == uid).ToList();
+            if (enrollments.Count == 0)
+            {
+                return Json(new { gpa = 0.0 });
+            }
+
+            double totalPoints = 0.0;
+            int totalCredits = 0;
+
+            // Calculate the total points and credits for the GPA
+            foreach (var enrollment in enrollments)
+            {
+                if (enrollment.Grade != null && enrollment.Grade != "--")
+                {
+                    double points = GetGradePoint(enrollment.Grade);
+                    totalPoints += points * 4.0; // Assuming all classes are 4 credit hours
+                    totalCredits += 4;
+                }
+            }
+
+            // Calculate the GPA
+            double gpa = totalCredits > 0 ? totalPoints / totalCredits : 0.0;
+
+            return Json(new { gpa });
         }
-                
-        /*******End code to modify********/
+
+        // Helper method to get the grade point value based on the letter grade
+        private double GetGradePoint(string grade)
+        {
+            switch (grade.ToUpper())
+            {
+                case "A":
+                    return 4.0;
+                case "A-":
+                    return 3.7;
+                case "B+":
+                    return 3.3;
+                case "B":
+                    return 3.0;
+                case "B-":
+                    return 2.7;
+                case "C+":
+                    return 2.3;
+                case "C":
+                    return 2.0;
+                case "C-":
+                    return 1.7;
+                case "D+":
+                    return 1.3;
+                case "D":
+                    return 1.0;
+                case "D-":
+                    return 0.7;
+                case "E":
+                    return 0.0;
+                default:
+                    return 0.0;
+            }
+        }
+
 
     }
 }
